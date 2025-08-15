1a30e678c446caefd87603fb3ef1935a22faad66
rdex999 <didabom2150@gmail.com>
2025-08-15 14:12:57 +0300

    Added documentation to the methods in LoginView

--- Client/Client/Views/LoginView.axaml.cs
+++ Client/Client/Views/LoginView.axaml.cs
@@ -1,5 +1,5 @@
-using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Client.ViewModels;
 
 namespace Client.Views;
@@ -9,20 +9,36 @@ public partial class LoginView : UserControl
 	public LoginView()
 	{
 		InitializeComponent();
-		this.Loaded += async (_, _) => await OnLoadedAsync();
+		this.Loaded += OnLoaded;
 	}
 
-	private async Task OnLoadedAsync()
+	/// <summary>
+	/// Handles the Loaded event - initializes LoginViewModel.
+	/// </summary>
+	/// <remarks>
+	/// Precondition: LoginView is loaded. <br/>
+	/// Postcondition: LoginViewModel started initialization procedure.
+	/// </remarks>
+	private void OnLoaded(object? sender, RoutedEventArgs e)
 	{
 		if (DataContext is LoginViewModel loginViewModel)
 		{
 			if (!loginViewModel.IsInitialized())
 			{
-				await loginViewModel.InitializeAsync();
+				_ = loginViewModel.InitializeAsync();
 			}
 		}
 	}
 
+	/// <summary>
+	/// Handles changes in the credentials input fields. (username, password)
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	/// <remarks>
+	/// Precondition: User has typed/deleted from the credentials input fields. <br/>
+	/// Postcondition: Change handled - buttons and errors are displayed accordingly. (username empty, etc)
+	/// </remarks>
 	private void OnCredentialsTextChanged(object? sender, TextChangedEventArgs e)
 	{
 		((LoginViewModel)DataContext!).OnCredentialsTextChanged();
