64cf9c5233bec056d544a0079234c949043bce53
rdex999 <didabom2150@gmail.com>
2025-08-15 14:25:32 +0300

    Added documentation to CreateAccountView.

--- Client/Client/Views/CreateAccountView.axaml.cs
+++ Client/Client/Views/CreateAccountView.axaml.cs
@@ -12,16 +12,43 @@ public partial class CreateAccountView : UserControl
 		InitializeComponent();
 	}
 
+	/// <summary>
+	/// Handles a change in password and password confirm input fields.
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	/// <remarks>
+	/// Precondition: User has deleted/typed something in password or password confirm input fields. <br/>
+	/// Postcondition: Errors and buttons are displayed as necessary. (For example, the "create account" button will not be visible if the passwords are not equal.)
+	/// </remarks>
 	private void OnPasswordTextChanged(object? sender, TextChangedEventArgs e)
 	{
 		((CreateAccountViewModel)DataContext!).PasswordTextChanged();
 	}
 
+	/// <summary>
+	/// Handles a change in the username input field.
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	/// <remarks>
+	/// Precondition: User has deleted/typed something in the username input field. <br/>
+	/// Postcondition: Errors and buttons are displayed as necessary. (For example, if there is a user with the inputted username, an error is displayed)
+	/// </remarks>
 	private async void OnUsernameTextChangedAsync(object? sender, TextChangedEventArgs e)
 	{
 		await ((CreateAccountViewModel)DataContext!).UsernameTextChangedAsync();
 	}
 
+	/// <summary>
+	/// Handles lost focus event in the password confirm input field. This is done to untoggle the reveal password button.
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	/// <remarks>
+	/// Precondition: The password confirm input field has lost focus. <br/>
+	/// Postcondition: The reveal password toggle button is untoggled.
+	/// </remarks>
 	private void PasswordConfirmTextBox_OnLostFocus(object? sender, RoutedEventArgs e)
 	{
 		PasswordStateButton.IsChecked = false;
