c5b66c3e3616cf4c7b185046a146c608da3cf02a
rdex999 <didabom2150@gmail.com>
2025-08-15 16:11:33 +0300

    Added documentation to the methods in HomeViewModel.

--- Client/Client/ViewModels/HomeViewModel.cs
+++ Client/Client/ViewModels/HomeViewModel.cs
@@ -6,7 +6,20 @@ namespace Client.ViewModels;
 public class HomeViewModel : ViewModelBase
 {
 	public ObservableCollection<VmItemTemplate> VMs { get; }
-	
+
+	/// <summary>
+	/// Initializes a new instance of HomeViewModel.
+	/// </summary>
+	/// <param name="navigationSvc">
+	/// The navigation service. navigationSvc != null.
+	/// </param>
+	/// <param name="clientSvc">
+	/// The client service. clientSvc != null.
+	/// </param>
+	/// <remarks>
+	/// Precondition: MainView is created, (HomeViewModel is the default side menu selection) or the user selects the home page in the side menu. <br/>
+	/// Postcondition: A new instance of HomeViewModel is created.
+	/// </remarks>
 	public HomeViewModel(NavigationService navigationSvc, ClientService clientSvc)
 		: base(navigationSvc, clientSvc)
 	{
@@ -23,11 +36,19 @@ public class VmItemTemplate
 	public string Name { get; set; }
 	public string OperatingSystem { get; }
 	public VmStatus Status { get; set; }
+	
+	/// <summary>
+	/// The status of the VM as a string.
+	/// </summary>
+	/// <remarks>
+	/// Precondition: No specific precondition. <br/>
+	/// Postcondition: A string representation with space-seperated words of the Status property is returned.
+	/// </remarks>
 	public string StatusString 
 	{
 		get
 		{
-			/* Seperate the words with spaces */
+			/* Separate the words with spaces */
 			string result = Status.ToString();
 			for (int i = 0; i < result.Length; i++)
 			{
@@ -41,6 +62,19 @@ public class VmItemTemplate
 		}
 	}
 
+	/// <summary>
+	/// Creates a new instance of a VmItemTemplate.
+	/// </summary>
+	/// <param name="name">
+	/// The name of the VM. name != null.
+	/// </param>
+	/// <param name="operatingSystem">
+	/// The operating system of the VM. operatingSystem != null.
+	/// </param>
+	/// <remarks>
+	/// Precondition: name != null &amp;&amp; operatingSystem != null. <br/>
+	/// Postcondition: A new instance of VmItemTemplate is created.
+	/// </remarks>
 	public VmItemTemplate(string name, string operatingSystem)
 	{
 		Name = name;
