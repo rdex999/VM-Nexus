3a154ab662a630515a793069c33fbec812a2dd47
rdex999 <didabom2150@gmail.com>
2025-09-05 16:11:29 +0300

    Now disabling and setting values to VM creation settings based on which operating system is selected.

--- Client/Client/ViewModels/CreateVmViewModel.cs
+++ Client/Client/ViewModels/CreateVmViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using Client.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -17,14 +18,51 @@ public partial class CreateVmViewModel : ViewModelBase
 	private int _osDriveSize = 20480;
 
 	[ObservableProperty] 
+	private int _osDriveSizeMax = 1024 * 256;
+	
+	[ObservableProperty]
+	private int _osDriveSizeMin = 1;
+	
+	[ObservableProperty]
+	private bool _osDriveSizeIsEnabled = true;
+
+	[ObservableProperty] 
 	private SharedDefinitions.DriveType _osDriveType = SharedDefinitions.DriveType.NVMe;
+	
+	[ObservableProperty]
+	private bool _osDriveTypeIsEnabled = true;
 
 	[ObservableProperty]
 	private SharedDefinitions.CpuArchitecture _cpuArchitecture = SharedDefinitions.CpuArchitecture.X86_64;
 	
+	[ObservableProperty]
+	private bool _cpuArchitectureIsEnabled = true;
+	
 	public CreateVmViewModel(NavigationService navigationSvc, ClientService clientSvc)
 		: base(navigationSvc, clientSvc)
 	{
+	}
+
+	public void OperatingSystemChanged()
+	{
+		if (OperatingSystem == SharedDefinitions.OperatingSystem.MiniCoffeeOs)
+		{
+			OsDriveSizeMax = 128;
+			OsDriveSizeMin = 1;
+			
+			OsDriveType =  SharedDefinitions.DriveType.Floppy;
+			OsDriveTypeIsEnabled = false;
+			
+			CpuArchitecture = SharedDefinitions.CpuArchitecture.X86;
+			CpuArchitectureIsEnabled = false;
+		}
+		else
+		{
+			OsDriveSizeMax = 1024 * 256;
+			OsDriveSizeMin = 1024 * 4;
+			OsDriveTypeIsEnabled = true;
+			CpuArchitectureIsEnabled = true;
+		}
 	}
 	
 	[RelayCommand]
--- Client/Client/Views/CreateVmView.axaml.cs
+++ Client/Client/Views/CreateVmView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Client.ViewModels;
 
 namespace Client.Views;
 
@@ -9,5 +10,13 @@ public partial class CreateVmView : UserControl
 	public CreateVmView()
 	{
 		InitializeComponent();
+	}
+
+	private void OperatingSystemChanged(object? sender, SelectionChangedEventArgs e)
+	{
+		if (DataContext is CreateVmViewModel vm)
+		{
+			vm.OperatingSystemChanged();
+		}
 	}
 }
