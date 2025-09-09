f70c1e791ffb52de8f23f701a2e907d7bf6fbe54
rdex999 <didabom2150@gmail.com>
2025-09-09 20:48:55 +0300

    Made a DeleteDriveAsync method in DatabaseService.

--- Server/Services/DatabaseService.cs
+++ Server/Services/DatabaseService.cs
@@ -435,6 +435,39 @@ public class DatabaseService
 	}
 
 	/// <summary>
+	/// Deletes the given drive from the database. (Not the disk image from the actual filesystem)
+	/// </summary>
+	/// <param name="username">The username of the user who owns the drive. username != null.</param>
+	/// <param name="name">The name of the drive. name != null.</param>
+	/// <returns>An exit code indicating the result of the operation.</returns>
+	/// <remarks>
+	/// Precondition: A user whos username is the given username - exists. The user has a drive which name is the given name.
+	/// username != null &amp;&amp; name != null. <br/>
+	/// Postcondition: On success, the drive is deleted from the database and the returned exit code indicates success. <br/>
+	/// On failure, the drive is not deleted from the database and the returned exit code indicates the error.
+	/// </remarks>
+	public async Task<ExitCode> DeleteDriveAsync(string username, string name)
+	{
+		int userId = await GetUserIdAsync(username);
+		if (userId == -1)
+		{
+			return ExitCode.UserDoesntExist;
+		}
+
+		int rows = await ExecuteNonQueryAsync("DELETE FROM drives WHERE owner_id = @owner_id AND name = @name",
+			new NpgsqlParameter("@owner_id", userId),
+			new NpgsqlParameter("@name", name)
+		);
+
+		if (rows >= 1)
+		{
+			return ExitCode.Success;
+		}
+		
+		return ExitCode.DatabaseOperationFailed;
+	}
+
+	/// <summary>
 	/// The asynchronous version of the ExecuteNonQuery command.
 	/// Executes a Non-Query command (Something that doesnt search for stuff, like a DELETE or INSERT command)
 	/// </summary>
