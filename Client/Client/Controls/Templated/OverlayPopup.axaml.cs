977be4ac50a132cec6c98e835fd24ac047b5a971
rdex999 <didabom2150@gmail.com>
2025-10-06 15:12:46 +0300

    Added Title property to OverlayPopup, and a close button that closes the popup.

--- Client/Client/Controls/Templated/OverlayPopup.axaml.cs
+++ Client/Client/Controls/Templated/OverlayPopup.axaml.cs
@@ -1,12 +1,16 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 using Avalonia.Metadata;
 
 namespace Client.Controls.Templated;
 
 public class OverlayPopup : TemplatedControl
 {
+	public event EventHandler? Closed;
+	
 	public static readonly StyledProperty<object?> ContentProperty = 
 		AvaloniaProperty.Register<OverlayPopup, object?>(nameof(Content));
 	
@@ -19,6 +23,9 @@ public class OverlayPopup : TemplatedControl
 	public static readonly StyledProperty<Control?> PlacementTargetProperty =
 		AvaloniaProperty.Register<OverlayPopup, Control?>(nameof(PlacementTarget));
 
+	public static readonly StyledProperty<string?> TitleProperty =
+		AvaloniaProperty.Register<OverlayPopup, string?>(nameof(Title));
+	
 	[Content]
 	public object? Content
 	{
@@ -43,5 +50,30 @@ public class OverlayPopup : TemplatedControl
 	{
 		get => GetValue(PlacementTargetProperty);
 		set => SetValue(PlacementTargetProperty, value);
+	}
+
+	public string? Title
+	{
+		get => GetValue(TitleProperty);
+		set => SetValue(TitleProperty, value);
+	}
+
+	public OverlayPopup()
+	{
+	}
+
+	protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+	{
+		base.OnApplyTemplate(e);
+		Button? closeButton = e.NameScope.Find<Button>("CloseButton");
+		if (closeButton == null) return;
+		
+		closeButton.Click += OnCloseButtonClick;
+	}
+
+	private void OnCloseButtonClick(object? sender, RoutedEventArgs e)
+	{
+		IsOpen = false;
+		Closed?.Invoke(this, EventArgs.Empty);
 	}
 }
