e157d6328c6b187a6961ed4f593ce22036b21a2c
rdex999 <didabom2150@gmail.com>
2025-11-04 21:36:20 +0200

    Made a path top bar in DriveExplorerView, started working on displaying drives.

--- Client/Client/ViewModels/DriveExplorerModes/DrivesViewModel.cs
+++ Client/Client/ViewModels/DriveExplorerModes/DrivesViewModel.cs
@@ -1,11 +1,30 @@
+using System.Collections.ObjectModel;
 using Client.Services;
+using Shared.Drives;
 
 namespace Client.ViewModels.DriveExplorerModes;
 
 public class DrivesViewModel : ViewModelBase
 {
+	public ObservableCollection<DriveItemTemplate> DriveItems { get; }
+	
 	public DrivesViewModel(NavigationService navigationService, ClientService clientService)
 		: base(navigationService, clientService)
 	{
+		DriveItems = new ObservableCollection<DriveItemTemplate>();
+		
+		/* For testing */
+		DriveItems.Add(new DriveItemTemplate(new DriveGeneralDescriptor(0, "drive1", 32000, DriveType.Disk)));
+		DriveItems.Add(new DriveItemTemplate(new DriveGeneralDescriptor(1, "drive2", 32000, DriveType.Floppy)));
+	}
+}
+
+public class DriveItemTemplate
+{
+	public DriveGeneralDescriptor Descriptor { get; }
+	
+	public DriveItemTemplate(DriveGeneralDescriptor descriptor)
+	{
+		Descriptor = descriptor;
 	}
 }
--- Client/Client/ViewModels/DriveExplorerViewModel.cs
+++ Client/Client/ViewModels/DriveExplorerViewModel.cs
@@ -1,14 +1,20 @@
 using Client.Services;
+using Client.ViewModels.DriveExplorerModes;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Client.ViewModels;
 
-public class DriveExplorerViewModel : ViewModelBase
+public partial class DriveExplorerViewModel : ViewModelBase
 {
 	private readonly DriveService _driveService;
+	
+	[ObservableProperty]
+	private ViewModelBase _explorerModeViewModel;
 	
 	public DriveExplorerViewModel(NavigationService navigationService, ClientService clientService, DriveService driveService)
 		: base(navigationService, clientService)
 	{
 		_driveService = driveService;
+		ExplorerModeViewModel = new DrivesViewModel(NavigationSvc, ClientSvc);
 	}
 }
