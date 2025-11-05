eb80d577781b01327229cf1c9f725bb9c115a69f
rdex999 <didabom2150@gmail.com>
2025-11-05 14:36:15 +0200

    Styled drives list in DrivesView.

--- Client/Client/ViewModels/DriveExplorerModes/DrivesViewModel.cs
+++ Client/Client/ViewModels/DriveExplorerModes/DrivesViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using Client.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Shared.Drives;
 
 namespace Client.ViewModels.DriveExplorerModes;
@@ -14,17 +15,44 @@ public class DrivesViewModel : ViewModelBase
 		DriveItems = new ObservableCollection<DriveItemTemplate>();
 		
 		/* For testing */
-		DriveItems.Add(new DriveItemTemplate(new DriveGeneralDescriptor(0, "drive1", 32000, DriveType.Disk)));
-		DriveItems.Add(new DriveItemTemplate(new DriveGeneralDescriptor(1, "drive2", 32000, DriveType.Floppy)));
+		DriveItems.Add(new DriveItemTemplate(0, "drive1", 26000, DriveType.Disk));
+		DriveItems.Add(new DriveItemTemplate(1, "drive2", 1024 * 32, DriveType.Disk));
+		DriveItems.Add(new DriveItemTemplate(2, "drive3", 260, DriveType.Floppy));
 	}
 }
 
-public class DriveItemTemplate
+public partial class DriveItemTemplate : ObservableObject
 {
-	public DriveGeneralDescriptor Descriptor { get; }
-	
-	public DriveItemTemplate(DriveGeneralDescriptor descriptor)
+	private int _id;
+
+	private int _size;
+
+	public int Size
 	{
-		Descriptor = descriptor;
+		get => _size;
+		set
+		{
+			_size = value;
+			SizeString = _size >= 1024 
+				? $"{(Size/1024.0):0.##} GiB" 
+				: $"{Size} MiB";
+		}
+	}
+
+	[ObservableProperty]
+	private string _name;
+
+	[ObservableProperty] 
+	private string _sizeString = null!;
+
+	[ObservableProperty] 
+	private DriveType _driveType;
+
+	public DriveItemTemplate(int id, string name, int size, DriveType driveType)
+	{
+		_id = id;
+		Name = name;
+		Size = size;
+		DriveType = driveType;
 	}
 }
