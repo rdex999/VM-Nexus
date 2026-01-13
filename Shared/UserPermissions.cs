9686a1ec65a2102c73e9274c1d81697c21472241
rdex999 <didabom2150@gmail.com>
2026-01-13 17:31:16 +0200

    Made GetIncluded and AddIncluded extension methods in UserPermissions.

--- Shared/UserPermissions.cs
+++ Shared/UserPermissions.cs
@@ -4,18 +4,18 @@ namespace Shared;
 public enum UserPermissions
 {
 	VirtualMachineList		= 1 << 0,
-	VirtualMachineCreate	= VirtualMachineList | (1 << 1),
-	VirtualMachineDelete	= VirtualMachineList | (1 << 2),
-	VirtualMachineWatch		= VirtualMachineList | (1 << 3),
-	VirtualMachineUse		= VirtualMachineWatch | (1 << 4),
+	VirtualMachineCreate	= 1 << 1,
+	VirtualMachineDelete	= 1 << 2,
+	VirtualMachineWatch		= 1 << 3,
+	VirtualMachineUse		= 1 << 4,
 	
 	DriveList				= 1 << 5,
-	DriveCreate				= DriveList | (1 << 6),
-	DriveDelete				= DriveList | (1 << 7),
-	DriveItemList			= DriveList | (1 << 8),
-	DriveItemCreate			= DriveItemList | (1 << 9),
-	DriveItemDelete			= DriveItemList | (1 << 10),
-	DriveItemDownload		= DriveItemList | (1 << 11),
+	DriveCreate				= 1 << 6,
+	DriveDelete				= 1 << 7,
+	DriveItemList			= 1 << 8,
+	DriveItemCreate			= 1 << 9,
+	DriveItemDelete			= 1 << 10,
+	DriveItemDownload		= 1 << 11,
 }
 
 public static class UserPermissionsExtensions
@@ -59,6 +59,57 @@ public static class UserPermissionsExtensions
 	public static UserPermissions RemovePermission(this UserPermissions permissions, UserPermissions permission) =>
 		permissions & ~permission;
 
+	/// <summary>
+	/// Get the permissions that the given permissions need in order to be valid. <br/>
+	/// For example, the VM use permission depends on the VM watch permission, which depends on the VM list permission.
+	/// </summary>
+	/// <param name="permissions">The current permissions.</param>
+	/// <returns>The permissions that the given permissions depend on.</returns>
+	/// <remarks>
+	/// Precondition: No specific precondition. <br/>
+	/// Postcondition: The permissions that the given permissions depend on are returned.
+	/// </remarks>
+	public static UserPermissions GetIncluded(this UserPermissions permissions)
+	{
+		UserPermissions result = 0;
+		UserPermissions[] prms = permissions.ToArray();
+
+		foreach (UserPermissions permission in prms)
+		{
+			result |= permission switch
+			{
+				UserPermissions.VirtualMachineCreate or
+					UserPermissions.VirtualMachineDelete or
+					UserPermissions.VirtualMachineWatch			=> UserPermissions.VirtualMachineList,
+
+				UserPermissions.VirtualMachineUse				=> UserPermissions.VirtualMachineList | UserPermissions.VirtualMachineWatch,
+
+				UserPermissions.DriveCreate or
+					UserPermissions.DriveDelete or
+					UserPermissions.DriveItemList				=> UserPermissions.DriveList,
+
+				UserPermissions.DriveItemCreate or
+					UserPermissions.DriveItemDelete or
+					UserPermissions.DriveItemDownload			=> UserPermissions.DriveItemList | UserPermissions.DriveList,
+
+				_ => 0
+			};
+		}
+
+		return result;
+	}
+
+	/// <summary>
+	/// Add the required permissions to the current ones, for them to be valid. (Add dependant permissions)
+	/// </summary>
+	/// <param name="permissions">The current permissions.</param>
+	/// <returns>The original permissions, along with the permissions they depend on.</returns>
+	/// <remarks>
+	/// Precondition: No specific precondition. <br/>
+	/// Postcondition: The original permissions along with the permissions they depend on are returned.
+	/// </remarks>
+	public static UserPermissions AddIncluded(this UserPermissions permissions) => permissions | permissions.GetIncluded();
+	
 	/// <summary>
 	/// Converts into a permission array, while each element is a single permission.
 	/// </summary>
