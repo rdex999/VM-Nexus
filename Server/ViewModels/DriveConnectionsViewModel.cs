1fb7323e58cb3f0935dcf03ab5136a5b17565625
rdex999 <didabom2150@gmail.com>
2026-01-26 21:55:38 +0200

    Implemented DriveConnectionsViewModel core logic.

--- Server/ViewModels/DriveConnectionsViewModel.cs
+++ Server/ViewModels/DriveConnectionsViewModel.cs
@@ -1,22 +1,41 @@
+using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Server.Services;
 using Shared;
 
 namespace Server.ViewModels;
 
-public class DriveConnectionsViewModel : ViewModelBase
+public partial class DriveConnectionsViewModel : ViewModelBase
 {
 	private readonly DatabaseService _databaseService;
+
+	[ObservableProperty]
+	private string _query = string.Empty;
+	
+	public ObservableCollection<DriveConnectionItemTemplate> DriveConnections { get; }
 	
 	public DriveConnectionsViewModel(DatabaseService databaseService)
 	{
 		_databaseService = databaseService;
+		DriveConnections = new ObservableCollection<DriveConnectionItemTemplate>();
+		_ = RefreshAsync();
 	}
 
 	/* Use for IDE preview only. */
 	public DriveConnectionsViewModel()
 	{
 		_databaseService = null!;
+		DriveConnections = new ObservableCollection<DriveConnectionItemTemplate>()
+		{
+			new DriveConnectionItemTemplate(1, "d", "test_vm0 - MiniCoffeeOS", 
+				"test_vm0", 5, 6, DateTime.Now),
+			
+			new DriveConnectionItemTemplate(1, "d", "test_vm1 - Manjaro", 
+				"test_vm1", 8, 9, DateTime.Now),
+		};
 	}
 	
 	/// <summary>
@@ -27,8 +46,42 @@ public class DriveConnectionsViewModel : ViewModelBase
 	/// Precondition: Either the user has clicked on the refresh button, or refreshing the drive connections list is needed. <br/>
 	/// Postcondition: On success, the list is updated according to latest data. On failure, the list is cleared.
 	/// </remarks>
+	[RelayCommand]
 	public async Task<ExitCode> RefreshAsync()
 	{
+		DriveConnections.Clear();
+		DatabaseService.SearchedDriveConnection[]? connections = await _databaseService.SearchDriveConnectionsAsync(Query);
+		if (connections == null)
+			return ExitCode.DatabaseOperationFailed;
+		
+		foreach (DatabaseService.SearchedDriveConnection connection in connections)
+			DriveConnections.Add(new DriveConnectionItemTemplate(connection));
+		
 		return ExitCode.Success;
+	}
+	
+	/// <summary>
+	/// Handles a change in the query field. Updates the drive connections list according to the set query.
+	/// </summary>
+	/// <param name="value">Unused.</param>
+	/// <remarks>
+	/// Precondition: The query field has changed - the user has changed its content. <br/>
+	/// Postcondition: A refresh of the drive connections list is started according to the set query.
+	/// </remarks>
+	partial void OnQueryChanged(string value) => _ = RefreshAsync();
+}
+
+public class DriveConnectionItemTemplate : DatabaseService.SearchedDriveConnection
+{
+	public DriveConnectionItemTemplate(int ownerId, string ownerUsername, string driveName, string virtualMachineName, 
+		int driveId, int virtualMachineId, DateTime connectedAt) 
+		: base(ownerId, ownerUsername, driveName, virtualMachineName, driveId, virtualMachineId, connectedAt)
+	{
+	}
+
+	public DriveConnectionItemTemplate(DatabaseService.SearchedDriveConnection connection)
+		: base(connection.OwnerId, connection.OwnerUsername, connection.DriveName, connection.VirtualMachineName,
+			connection.DriveId, connection.VirtualMachineId, connection.ConnectedAt)
+	{
 	}
 }
