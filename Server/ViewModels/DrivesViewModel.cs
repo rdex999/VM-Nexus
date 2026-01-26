e100aa8d85e089d6f68848c5dce237aa8a091724
rdex999 <didabom2150@gmail.com>
2026-01-26 20:03:53 +0200

    Implemented DrivesViewModel general logic.

--- Server/ViewModels/DrivesViewModel.cs
+++ Server/ViewModels/DrivesViewModel.cs
@@ -1,15 +1,26 @@
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Server.Services;
+using Shared;
+using Shared.Drives;
 
 namespace Server.ViewModels;
 
-public class DrivesViewModel : ViewModelBase
+public partial class DrivesViewModel : ViewModelBase
 {
-	private DatabaseService _databaseService;
+	private readonly DatabaseService _databaseService;
+	
+	public ObservableCollection<DriveItemTemplate> Drives { get; }
+
+	[ObservableProperty]
+	private string _query = string.Empty;
 
 	public DrivesViewModel(DatabaseService databaseService)
 	{
 		_databaseService = databaseService;
+		Drives = new ObservableCollection<DriveItemTemplate>();
 		_ = RefreshAsync();
 	}
 
@@ -17,6 +28,12 @@ public class DrivesViewModel : ViewModelBase
 	public DrivesViewModel()
 	{
 		_databaseService = null!;
+
+		Drives = new ObservableCollection<DriveItemTemplate>()
+		{
+			new DriveItemTemplate(new DatabaseService.SearchedDrive(1, 5, "d", "test_vm0 - MiniCoffeeOS", 13, DriveType.Floppy)),
+			new DriveItemTemplate(new DatabaseService.SearchedDrive(2, 6, "david", "test_vm1 - Manjaro", 30000, DriveType.Disk)),
+		};
 	}
 	
 	/// <summary>
@@ -27,7 +44,49 @@ public class DrivesViewModel : ViewModelBase
 	/// Precondition: Either the user has clicked on the refresh button, or refreshing the drives list is needed. <br/>
 	/// Postcondition: On success, the list is updated according to latest data. On failure, the list is cleared.
 	/// </remarks>
-	public async Task RefreshAsync()
+	[RelayCommand]
+	public async Task<ExitCode> RefreshAsync()
 	{
+		DatabaseService.SearchedDrive[]? drives = await _databaseService.SearchDrivesAsync(Query);
+		if (drives == null)
+			return ExitCode.DatabaseOperationFailed;
+		
+		foreach (DatabaseService.SearchedDrive drive in drives)
+			Drives.Add(new DriveItemTemplate(drive));
+
+		return ExitCode.Success;
+	}
+	
+	/// <summary>
+	/// Handles a change in the query field. Updates the drives list according to the set query.
+	/// </summary>
+	/// <param name="value">Unused.</param>
+	/// <remarks>
+	/// Precondition: The query field has changed - the user has changed its content. <br/>
+	/// Postcondition: A refresh of the drives list is started according to the set query.
+	/// </remarks>
+	partial void OnQueryChanged(string value) => _ = RefreshAsync();
+}
+
+public class DriveItemTemplate
+{
+	public int Id { get; }
+	public int OwnerId { get; }
+	public string OwnerUsername { get; }
+	public string Name { get; }
+	public string Size { get; }
+	public DriveType DriveType { get; }
+
+	public DriveItemTemplate(DatabaseService.SearchedDrive drive)
+	{
+		Id = drive.Id;
+		OwnerId = drive.OwnerId;
+		OwnerUsername = drive.OwnerUsername;
+		Name = drive.Name;
+		Size = 	drive.SizeMiB < 1024
+			? $"{drive.SizeMiB} MiB"
+			: $"{(drive.SizeMiB / 1024.0):0.##} GiB";
+		
+		DriveType = drive.DriveType;
 	}
 }
