6662b644bd83349de32c465617d1a30fd6ba544a
rdex999 <didabom2150@gmail.com>
2026-01-26 13:36:36 +0200

    Designed UsersView in server.

--- Server/ViewModels/UsersViewModel.cs
+++ Server/ViewModels/UsersViewModel.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.ObjectModel;
 using Server.Services;
 
 namespace Server.ViewModels;
@@ -5,9 +7,45 @@ namespace Server.ViewModels;
 public class UsersViewModel : ViewModelBase
 {
 	private readonly DatabaseService _databaseService;
+	
+	public ObservableCollection<UserItemTemplate> Users { get; }
 
 	public UsersViewModel(DatabaseService databaseService)
 	{
 		_databaseService = databaseService;
+		Users = new ObservableCollection<UserItemTemplate>();
+	}
+
+	/* Use for IDE preview only. */
+	public UsersViewModel()
+	{
+		_databaseService = null!;
+		Users = new ObservableCollection<UserItemTemplate>()
+		{
+			new UserItemTemplate(1, "d", "d@gmail.com", DateTime.Now, null, null),
+			new UserItemTemplate(2, "child", "child@gmail.com", DateTime.Now, 1, "d"),
+		};
+	}
+}
+
+public class UserItemTemplate
+{
+	public int Id { get; }
+	public string Username { get; }
+	public string Email { get; }
+	public DateTime CreatedAt { get; }
+	public bool IsSubUser { get; }
+	public int? OwnerId { get; }
+	public string? OwnerUsername { get; }
+
+	public UserItemTemplate(int id, string username, string email, DateTime createdAt, int? ownerId, string? ownerUsername)
+	{
+		Id = id;
+		Username = username;
+		Email = email;
+		CreatedAt = createdAt;
+		IsSubUser = ownerId.HasValue;
+		OwnerId = ownerId;
+		OwnerUsername = ownerUsername;
 	}
 }
